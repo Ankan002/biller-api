68bb7297469cd6097517f34dd94f98ee916a2d44
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-19 19:18:14 +0530

    chore: :wrench: add winston

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express from "express";
+import { logger } from "utils/logger";
 
 export const startApp = () => {
-    const app = express();
-    const PORT = process.env["PORT"];
+	const app = express();
+	const PORT = process.env["PORT"];
 
-    app.get("/", (req, res) => {
-        return res.status(200).json({
-            success: true,
-            message: "Welcome to Biller API!!",
-        });
-    });
+	app.get("/", (req, res) => {
+		return res.status(200).json({
+			success: true,
+			message: "Welcome to Biller API!!",
+		});
+	});
 
-    app.listen(PORT, () => console.log(`App is running at ${PORT}`));
+	app.listen(PORT, () => logger.info(`App is running at ${PORT}`));
 };
