da0e1d6a7c970678f7b717f26fb431afdb0e609a
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-19 19:22:23 +0530

    chore: :wrench: add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import { logger } from "utils/logger";
+import cors from "cors";
+import { origins } from "constants/origins";
 
 export const startApp = () => {
 	const app = express();
 	const PORT = process.env["PORT"];
 
 	app.use(express.json());
+	app.use(
+		cors({
+			credentials: true,
+			origin: origins,
+		})
+	);
 
 	app.get("/", (req, res) => {
 		return res.status(200).json({
---
+++ src/constants/origins.ts
@@ -0,0 +1,10 @@
+export const origins =
+	process.env["NODE_ENV"] === "production"
+		? []
+		: [
+				"http://localhost:8000",
+				"http://localhost:8001",
+				"http://localhost:3000",
+				"http://localhost:3001",
+				"http://localhost:3002",
+		  ];
